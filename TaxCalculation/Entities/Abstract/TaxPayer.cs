634280dd91635813ca50201cb86e12ccb18907f4
gunn3r71 <lucasgunner3@gmail.com>
2021-05-19 23:12:55 -0300

    Implementando classe abstrata TaxPayer

---
+++ TaxCalculation/Entities/Abstract/TaxPayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxCalculation.Entities.Abstract
+{
+    public abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public decimal AnnualIncome { get; set; }
+
+        public abstract decimal TaxCalculation();
+    }
+}
